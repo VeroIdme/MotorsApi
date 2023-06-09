9bc7349469927ea2caef0d1cb13647dd21c0d714
VeroIdme <veronicaidme@gmail.com>
2023-06-08 21:26:04 -0500

    Updated

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
+//!😁😁Importacion de cors para que el servidor pueda aceptar peticiones de diferentes dominios
 const cors = require("cors")
+//!😁😁Importancion de express
 const express = require("express")
 
+//Guardamos la variable de express en app
 const app = express()
 
+//?Middleware para que la app pueda aceptar formato json
+app.use(express.json())
+
+//Importacion de rutas segun usuario
 const userRouter = require('./routes/user.routes')
 const repairRouter = require('./routes/repair.routes')
 
-app.use(express.json())
-
+//?Middleware para concatenar rutas e ir a los archivos routes 
 app.use("/api/v1/products", userRouter)
 app.use("/api/v1/products", repairRouter)
 
-
+//Exportacion de app 
 module.exports = app
--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
+//!😁😁Importacion y configuracion de dotenv para las variables de entorno
 require("dotenv").config()
+//!😁😁Importacion de app
 const app = require("./app")
+//!😁😁Importacion de la db
 const { db } = require("./database/config")
 
+
+//Autenticando y sincronizando base de datos
 db.authenticate()
     .then(() => console.log("Database authenticated"))
     .catch((err) => console.log(err))
@@ -10,6 +15,8 @@ db.sync()
     .then(() => console.log("Database synced"))
     .catch(err => console.log(err))
 
+
+//?El metodo listen, trayendo variables de entorno el PORT
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
